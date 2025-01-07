b6c3e9a00ef19cfbdf1531c64519de46722e3e93
Eien <Eien@EIEN>
2025-01-08 00:32:24 +0800

    加入專案檔案。

---
+++ MouseClicker/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace MouseClicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        
+
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStartStop2 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnStartStop2);
+            this.panel1.Location = new System.Drawing.Point(19, 73);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(768, 210);
+            this.panel1.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(23, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(421, 159);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(469, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(272, 76);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "目前狀態";
+            // 
+            // btnStartStop2
+            // 
+            this.btnStartStop2.AutoSize = true;
+            this.btnStartStop2.BackColor = System.Drawing.SystemColors.Info;
+            this.btnStartStop2.Font = new System.Drawing.Font("新細明體", 30F);
+            this.btnStartStop2.Location = new System.Drawing.Point(506, 131);
+            this.btnStartStop2.Name = "btnStartStop2";
+            this.btnStartStop2.Size = new System.Drawing.Size(205, 60);
+            this.btnStartStop2.TabIndex = 4;
+            this.btnStartStop2.Text = "停止中";
+            this.btnStartStop2.Click += new System.EventHandler(this.btnStartStop2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(31, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(742, 76);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "按下Pause/Break控制開關";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(810, 311);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("新細明體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "滑鼠連點程式";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label btnStartStop2;
+    }
+}
+
---
+++ MouseClicker/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseClicker
+{
+    public partial class Form1 : Form
+    {
+        // 引入外部函式來模擬滑鼠點擊
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        private Thread clickThread;
+        private bool clicking = false;
+
+        // 引入全局鍵盤鉤子的外部函式
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int VK_PAUSE = 0x13;
+
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // 設定全局鍵盤鉤子
+            _hookID = SetHook(_proc);
+            // 設定快捷鍵
+            //this.KeyPreview = true;
+            //this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+        }
+        private void btnStartStop2_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private void ToggleClicking()
+        {
+            if (!clicking)
+            {
+                // 開始連點
+                clicking = true;
+                clickThread = new Thread(() => ClickMouse(Cursor.Position.X, Cursor.Position.Y, 10));
+                clickThread.Start();
+                btnStartStop2.Text = "連點中";
+            }
+            else
+            {
+                // 停止連點
+                clicking = false;
+                if (clickThread != null && clickThread.IsAlive)
+                {
+                    clickThread.Join();
+                }
+                btnStartStop2.Text = "停止中";
+            }
+        }
+        private void ClickMouse(int x, int y, int interval)
+        {
+            // 設定滑鼠位置
+            Cursor.Position = new System.Drawing.Point(x, y);
+
+            while (clicking)
+            {
+                // 模擬滑鼠按下和釋放
+                mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+                mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+
+                // 等待指定的間隔時間
+                Thread.Sleep(interval);
+            }
+        }
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (var curProcess = System.Diagnostics.Process.GetCurrentProcess())
+            using (var curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                if (vkCode == VK_PAUSE)
+                {
+                    // 切換連點狀態
+                    ((Form1)Application.OpenForms[0]).ToggleClicking();
+                }
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            // 釋放全局鍵盤鉤子
+            UnhookWindowsHookEx(_hookID);
+            // 停止連點
+            clicking = false;
+            if (clickThread != null && clickThread.IsAlive)
+            {
+                clickThread.Join();
+            }
+            base.OnFormClosed(e);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
